56662c58709ddafbc481cc20977f02660cec8b01
yxssz <yxs771129>
2019-02-17 16:55:08 +0800

    Merge branch 'master' of https://github.com/riozenc/titan-idea
    # Conflicts:
    #	gateway/src/main/java/org/gateway/config/RouteConfiguration.java

--- gateway/src/main/java/org/gateway/config/CorsConfig.java
+++ gateway/src/main/java/org/gateway/config/CorsConfig.java
@@ -5,6 +5,7 @@
  **/
 package org.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -19,10 +21,13 @@ import org.springframework.web.server.WebFilterChain;
 
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 	private static final String MAX_AGE = "18000L";
-
+	@Autowired
+	private RestTemplate restTemplate;
 	@Bean
 	public WebFilter corsFilter() {
 		return (ServerWebExchange ctx, WebFilterChain chain) -> {
@@ -45,7 +50,23 @@ public class CorsConfig {
 					response.setStatusCode(HttpStatus.OK);
 					return Mono.empty();
 				}
+				else if(request.getHeaders().containsKey("Authorization")){
+					List<String> list = request.getHeaders().get("Authorization");
+					String value = "";
+					for (int i=0; i<list.size(); i++){
+						value = list.get(i);
+						if(value.toLowerCase().startsWith("Bearer".toLowerCase())){
+							break;
+						}
+					}
+					if(value.isEmpty()){
+						response.setStatusCode(HttpStatus.PROXY_AUTHENTICATION_REQUIRED);
+						return Mono.empty();
+					}
+					String userInfo = restTemplate.getForObject("http://AUTH-CENTER/auth/extractToken", String.class,value);
 
+					System.out.println(userInfo);
+				}
 			}
 			return chain.filter(ctx);
 		};
